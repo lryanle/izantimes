a241ff7e635451e42a112cbfd8dfd46289c5bd76
Ryan Le <lryanle@users.noreply.github.com>
2019-04-04 02:24:02 -0500

    pages rollout

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-$(function() {
-  if ($('#sticky-sidebar').length) {
-    var el = $('#sticky-sidebar');
-    var stickyTop = $('#sticky-sidebar').offset().top;
-    var stickyHeight = $('#sticky-sidebar').height();
-    $(window).scroll(function() {
-      var limit = $('#footer-wrapper').offset().top - stickyHeight - 20;
-      var windowTop = $(window).scrollTop();
-      if (stickyTop < windowTop) {
-        el.css({
-          position: 'fixed',
-          top: 20
-        });
-      } else {
-        el.css('position', 'static');
-      }
-      if (limit < windowTop) {
-        var diff = limit - windowTop;
-        el.css({
-          top: diff
-        });
-      }
-    });
-  }
-});
-
 // Back to top button
 (function() {
   $(document).ready(function() {
